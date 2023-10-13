ade937c722fa48b50b70674b7cec3e3141bc75fc
Baxodir <Baxodir@gmail.com>
2023-10-13 17:22:36 +0500

    profile/chat request completed

--- iChat/Controllers/ChatRequestViewController.swift
+++ iChat/Controllers/ChatRequestViewController.swift
@@ -1,45 +1,46 @@
 //
-//  ProfileViewController.swift
+//  ChatRequestViewController.swift
 //  iChat
 //
-//  Created by Baxodir Yuldashev  on 12/10/23.
+//  Created by Baxodir Yuldashev  on 13/10/23.
 //
 
 import UIKit
-import SnapKit
 
-class ProfileViewController: UIViewController {
+class ChatRequestViewController: UIViewController {
     
     let containerView = UIView()
     let imageView = UIImageView(image: UIImage(named: "human3"), contentMode: .scaleAspectFill)
     let nameLabel = UILabel(text: "text", font: .systemFont(ofSize: 20, weight: .light))
-    let aboutLabel = UILabel(text: "kawdbkawd jhbdjahw habwjabwd", font: .systemFont(ofSize: 16, weight: .light))
-    let myTextField = InsertableTextField()
+    let aboutLabel = UILabel(text: "You have the opportunity to start a new chat", font: .systemFont(ofSize: 16, weight: .light))
+    let acceptButton = UIButton(title: "ACCEPT", backgroundcolor: .black, font: .laoSangamMN20(), isShadow: false, cornerRadius: 10, titleColor: .white)
+    let denyButton = UIButton(title: "Deny", backgroundcolor: .mainWhite(), font: .laoSangamMN20(), isShadow: false, cornerRadius: 10, titleColor: UIColor(red: 213/255, green: 51/255, blue: 51/255, alpha: 1))
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .mainWhite()
         setupConstraints()
     }
-    
 }
 
-extension ProfileViewController {
+extension ChatRequestViewController {
     private func setupConstraints(){
+        
+        denyButton.layer.borderWidth = 1.2
+        denyButton.layer.borderColor = UIColor(red: 213/255, green: 51/255, blue: 51/255, alpha: 1).cgColor
         aboutLabel.numberOfLines = 0
         containerView.backgroundColor = .mainWhite()
         containerView.layer.cornerRadius = 30
-        
-        if let button = myTextField.rightView as? UIButton {
-            button.addTarget(self, action: #selector(sendMessage), for: .touchUpInside)
-        }
-        
+
         view.addSubview(imageView)
         view.addSubview(containerView)
         containerView.addSubview(nameLabel)
         containerView.addSubview(aboutLabel)
-        containerView.addSubview(myTextField)
         
+        let buttonsStackview = UIStackView(arrangedSubviews: [acceptButton, denyButton], axis: .horizontal, spacing: 7)
+        buttonsStackview.distribution = .fillEqually
+        containerView.addSubview(buttonsStackview)
+//
         imageView.snp.makeConstraints { make in
             make.left.right.top.equalToSuperview()
             make.bottom.equalTo(containerView.snp.top).offset(30)
@@ -56,11 +57,16 @@ extension ProfileViewController {
             make.top.equalTo(nameLabel.snp.bottom).offset(8)
             make.left.right.equalToSuperview().inset(24)
         }
-        myTextField.snp.makeConstraints { make in
-            make.top.equalTo(aboutLabel.snp.bottom).offset(8)
+        buttonsStackview.snp.makeConstraints { make in
+            make.top.equalTo(aboutLabel.snp.bottom).offset(24)
             make.left.right.equalToSuperview().inset(24)
-            make.height.equalTo(48)
+            make.height.equalTo(56)
         }
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        self.acceptButton.applyGradients(cornerRadius: 10)
     }
     
     @objc private func sendMessage() {
